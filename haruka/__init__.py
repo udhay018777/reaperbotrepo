8094a2bfff3aa763ef3107c4db6ea1dadda47a74
udhay018777 <63841073+udhay018777@users.noreply.github.com>
2020-05-02 21:55:25 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -27,7 +27,7 @@ if ENV:
 
     MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
-
+    STRICT_GBAN = os.environ.get('STRICT_GBAN', None)
     try:
         SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
     except ValueError:
